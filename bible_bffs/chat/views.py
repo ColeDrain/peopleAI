98de2abce2417e2eec128c96294a5f88567bd4de
Ugochukwu Onyebuchi <ugochukwu.onyebuchi@indicina.co>
2023-03-12 21:47:40 +0100

    chore: improvements

--- bible_bffs/chat/views.py
+++ bible_bffs/chat/views.py
@@ -5,9 +5,12 @@ from django.views.decorators.csrf import csrf_exempt
 import openai, os
 
 from django.utils import timezone
-from datetime import datetime, timedelta
+from datetime import timedelta
+from uuid import uuid4
+from django.contrib.sessions.backends.db import SessionStore
 
 from .models import Character, Chat
+from django.contrib.auth.models import User
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -20,8 +23,21 @@ def character_list(request):
 
 def character_chat(request, character_id):
     character = get_object_or_404(Character, id=character_id)
-    messages = Chat.objects.filter(character=character, user=request.user).order_by('timestamp')
+
+    # Get the current user, or create a new user if the user is not authenticated
+    if request.user.is_authenticated:
+        user = request.user
+    else:
+        user_id = request.session.get('user_id')
+        if user_id:
+            user = User.objects.filter(id=user_id).first()
+        else:
+            user = User.objects.create_user(str(uuid4()))
+            request.session['user_id'] = user.id
+
+    messages = Chat.objects.filter(character=character, user=user).order_by('timestamp')
     return render(request, 'chat/chat.html', {'character': character, 'messages': messages})
+
 
 
 @csrf_exempt
@@ -34,17 +50,25 @@ def send_message(request, character_id):
         # Get the OpenAI Chatbot response
         openai.api_key = os.getenv("OPENAI_API_KEY")
 
+        # Get or create the user for the current session
+        user_id = request.session.get('user_id')
+        if user_id:
+            user = User.objects.filter(id=user_id).first()
+        else:
+            user = User.objects.create_user(str(uuid4()))
+            request.session['user_id'] = user.id
+
         # Memory is limited to only messages sent in last 3hrs
         three_hours_ago = timezone.now() - timedelta(hours=3)
         today_messages = Chat.objects.filter(
-            character=character, user=request.user,
+            character=character, user=user,
             timestamp__gte=three_hours_ago,
             timestamp__lte=timezone.now()
         ).order_by('timestamp')
 
         system_message = {
             "role": "system", 
-            "content": f"This is a conversational chat! You'll be assuming the persona of {character.name} from the Bible and responding to the user's messages in character. Use language and tone consistent with the time period, and avoid referencing or answering any events or questions about characters after {character.name}'s death. Keep your responses conversational and stay in character."
+            "content": f"This is a conversational chat! You'll be assuming the persona of {character.name} from the Bible and responding to the user's messages in character. Use language and tone consistent with the time period, and avoid referencing or answering any events or questions about characters after {character.name}'s death. Keep your responses conversational and stay in character. Don't switch to any other character for any reason, never put AI or Language Model in your response, respond only as {character.name}"
         }
 
         previous_messages = []
@@ -63,18 +87,32 @@ def send_message(request, character_id):
         chatbot_response = response["choices"][0]["message"]["content"]
 
         
-        chat = Chat.objects.create(character=character, user=request.user, user_input=message_text, character_response=chatbot_response)
+        chat = Chat.objects.create(character=character, user=user, user_input=message_text, character_response=chatbot_response)
 
         return JsonResponse({'bot_response': chatbot_response})
         
-        # if chat:
-        #     character = get_object_or_404(Character, id=character_id)
-
-        #     # Get the messages for the specified character
-        #     messages = Chat.objects.filter(character=character, user=request.user)
-
-        #     # return template fragment with all the messages between user and character
-        #     return render(request, 'chat/message_list.html', {'messages': messages})
-        
     else:
         return HttpResponseBadRequest()
+
+
+
+import csv
+from django.http import HttpResponse
+from django.db import connection
+
+def export_csv(request, table_name):
+    # Get the SQLite table data
+    cursor = connection.cursor()
+    cursor.execute(f"SELECT * FROM {table_name}")
+    table_data = cursor.fetchall()
+
+    # Create the HttpResponse object with CSV header
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Disposition'] = f'attachment; filename="{table_name}.csv"'
+
+    # Write the table data to the response as CSV
+    writer = csv.writer(response)
+    writer.writerow([col[0] for col in cursor.description]) # Write column headers
+    writer.writerows(table_data)
+
+    return response
