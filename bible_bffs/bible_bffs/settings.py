c165544975f941f3d113046291965c96e492b99f
Ugochukwu Onyebuchi <ugochukwu.onyebuchi@indicina.co>
2023-03-18 01:17:29 +0100

    build: modify database config

--- bible_bffs/bible_bffs/settings.py
+++ bible_bffs/bible_bffs/settings.py
@@ -82,10 +82,7 @@ WSGI_APPLICATION = 'bible_bffs.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default='postgresql://postgres:postgres@localhost:5432/bbffs',
-        conn_max_age=600
-    )
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
 }
 
 
