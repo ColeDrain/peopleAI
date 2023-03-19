307f535fc1dc4e8059605c7a09665c3dbfc196bb
Ugochukwu Onyebuchi <ugochukwu.onyebuchi@indicina.co>
2023-03-19 16:54:45 +0100

    chore: modify root path

--- chat/urls.py
+++ chat/urls.py
@@ -4,6 +4,6 @@ from .views import character_list, character_chat, send_message
 
 urlpatterns = [
     path('', character_list, name='character_list'),
-    path('<int:character_id>/', character_chat, name='character_chat'),
+    path('chat/<int:character_id>/', character_chat, name='character_chat'),
     path('<int:character_id>/send-message/', send_message, name='send_message'),
 ]
