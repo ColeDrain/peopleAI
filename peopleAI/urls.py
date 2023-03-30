2e9ce9daf67c2db745216ca3af5a08b04ceb92c1
Ugochukwu Onyebuchi <ugochukwu.onyebuchi@indicina.co>
2023-03-30 21:53:41 +0100

    chore: update admin path

--- peopleAI/urls.py
+++ peopleAI/urls.py
@@ -20,5 +20,5 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('chat.urls')),
-    path('admin/', admin.site.urls),
+    path('secret-hq-2023/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
